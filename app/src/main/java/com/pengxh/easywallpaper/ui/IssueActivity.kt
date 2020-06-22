3a1041039efead412ec2e41d99ea6b8cc8df4e5a
pengxianhong <290677893@qq.com>
2020-06-22 23:58:07 +0800

    问题反馈界面

--- app/src/main/java/com/pengxh/easywallpaper/ui/IssueActivity.kt
+++ app/src/main/java/com/pengxh/easywallpaper/ui/IssueActivity.kt
@@ -1,7 +1,12 @@
 package com.pengxh.easywallpaper.ui
 
+import android.graphics.Color
+import android.view.View
+import com.gyf.immersionbar.ImmersionBar
 import com.pengxh.app.multilib.base.BaseNormalActivity
 import com.pengxh.easywallpaper.R
+import com.pengxh.easywallpaper.utils.StatusBarColorUtil
+import kotlinx.android.synthetic.main.include_title.*
 
 /**
  * @author: Pengxh
@@ -14,10 +19,16 @@ class IssueActivity : BaseNormalActivity() {
     override fun initLayoutView(): Int = R.layout.activity_issue
 
     override fun initData() {
+        StatusBarColorUtil.setColor(this, Color.WHITE)
+        ImmersionBar.with(this).statusBarDarkFont(true, 0.2f).init()
 
+        mTitleView.text = "问题反馈"
+        mTitleRightView.visibility = View.GONE
     }
 
     override fun initEvent() {
-
+        mTitleLeftView.setOnClickListener {
+            this.finish()
+        }
     }
 }
